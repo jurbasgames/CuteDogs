925c1aee824a2827bee1bd296a45eeaf4b00de1b
João Pedro Duarte <jpduarte@ejcm.com.br>
2021-11-26 12:45:50 -0300

    feature: added Slider component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
@@ -8,14 +8,33 @@ import {
   Profile
 } from './src/screens'
 import { Entypo } from '@expo/vector-icons'
+import api from './src/services/api'
 
 const Tab = createBottomTabNavigator();
 
+interface Deck {
+  id: number;
+  url: string;
+  categories?: any;
+  breeds?: any;
+}
+
+
 export default function App() {
+  const [deck, setDeck] = useState<Deck[]>([]);
+
+    useEffect(() => {
+        api.get('/images/search?limit=10').then(response => {
+            console.log(response.data)
+            setDeck(response.data);
+        })
+    }, []);
   return (
     <NavigationContainer >
-      <Tab.Navigator screenOptions={{
+      <Tab.Navigator initialRouteName={"Profile"}
+      screenOptions={{
         headerShown: false,
+        
         tabBarActiveTintColor: '#305865',
         tabBarInactiveTintColor: '#112025',
         tabBarStyle:{
@@ -23,6 +42,7 @@ export default function App() {
           padding:5,
         }
       }}
+      
         >
       
         <Tab.Screen 
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, ImageBackground, Dimensions } from "react-native";
+import {Img} from 'react-image'
 
-
-export default function Card({ card }) {
+export default function Card( card: string ) {
+    let url = "https://cdn2.thedogapi.com/images/SJ6f2g9EQ_1280.jpg"
+    console.log(card)
     return (
         <View style={styles.card}>
-            <Image source={card.url} style={styles.cardImage} />
+            <Img src={card.card} style={{width: Dimensions.get('window').width,height:Dimensions.get('window').width, resizeMode: 'contain'}}//style={styles.cardImage} 
+            />
         </View>
     )
 };
@@ -13,8 +16,9 @@ export default function Card({ card }) {
 const styles = StyleSheet.create({
     cardImage: {
       width: 160,
+      height: 160,
       flex: 1,
-      resizeMode: 'contain'
+      resizeMode: 'stretch'
     },
     card: {
       flex: 0.45,
